ea4092bc1be56c626e032b51d9f066189b27553a
muhammad <31498696+moli1369@users.noreply.github.com>
2024-02-19 11:18:37 +0330

    Update Flask-web service.py
    update lang.

--- Flask-web service.py
+++ Flask-web service.py
@@ -3,14 +3,17 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # وضعیت اولیه چراغ راهنمایی (مقدار پیش‌فرض)
+# Ausgangszustand der Ampel (Standardwert)
 traffic_light_status = "RED"
 
 # روت برای دریافت وضعیت فعلی چراغ راهنمایی
+# Rooten Sie, um den aktuellen Ampelstatus abzurufen
 @app.route('/status', methods=['GET'])
 def get_traffic_light_status():
     return jsonify({"status": traffic_light_status})
 
 # روت برای تنظیم وضعیت چراغ راهنمایی
+# Rooten Sie, um den Ampelstatus einzustellen
 @app.route('/set_status', methods=['POST'])
 def set_traffic_light_status():
     global traffic_light_status
