05e78b83f7cfc0cc0f78d9e672d3514b185928a9
muhammad <31498696+moli1369@users.noreply.github.com>
2024-02-19 11:20:41 +0330

    Update decision.py

--- decision.py
+++ decision.py
@@ -1,4 +1,7 @@
 def decide_traffic_light(image):
     # اینجا کد تصمیم‌گیری بر اساس تصویر گرفته شده قرار می‌گیرد
+    # Hier wird der Entscheidungscode auf Basis des aufgenommenen Bildes platziert
     # ...
-    return "GREEN"  # به طور موقت مقدار ثابتی برمی‌گردانیم
+    return "GREEN" 
+    # به طور موقت مقدار ثابتی برمی‌گردانیم
+    #Gibt temporär einen festen Wert zurück
